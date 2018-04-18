73c1ecc71171896299bc2a5960663dcedd5c910f
yaoshifeng <1320701765@qq.com>
2018-04-18 14:27:15 +0800

    beautifulsoup和requests
    4月14号——spider_2.0.py
    学习了HTML的对应的标签
    初学爬虫的第一个demo，只是单纯通过正则表达式查找并取得对应的字符串，技术含量有点低....
    
    4月16号——spider_2.1.py
    在上一次知道爬取的流程后对2.0进行改良，通过结合Beautifulsoup库查找对应的内容，查找更准确
    
    4月19号——把之前看的CDSN博客里的mzitu反扒取功能添加，
    getip.py——http://www.xicidaili.com/wt/ 代理服务器可用列表，每次选取其中几页开始选定可用的
    国内代理的匿名服务器进行对应网页访问
    download.py——利用getip得到的代理服务器ip，重对request函数进行后改良，添加代理功能

---
+++ download.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env Python
+# coding=utf-8
+import requests
+import re
+import random
+import time
+import sys
+from getip import get_ip
+url = 'http://www.xicidaili.com/wt/'
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie':
+        '_free_proxy_session=BAh7B0'
+        'kiD3Nlc3Npb25faWQGOgZFVEkiJTZjNzMyYTI1ZTRhZjUwOWFhZmM0MzM3ZDE2OTgxYWE3BjsAVEk'
+        'iEF9jc3JmX3Rva2VuBjsARkkiMW56dzJ1TEtEcnhKU295aUZJS0UwOEdrN0FPVTErVlZFNm8xTn'
+        'IxaGRXRE09BjsARg%3D%3D--982dd8207a0b42a56678b7a98f0aa1aa02dd4429;'
+        'Hm_lvt_0cf76c77469e965d2957f0553e6ecf59=1516428973,1516430797; '
+        'Hm_lpvt_0cf76c77469e965d2957f0553e6ecf59=1516436070',
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/63.0.3239.132 '
+        'Safari/537.36'
+}
+
+
+class download:
+    def __init__(self):
+        self.ip_list = []
+        get_ip(url, headers, self.ip_list)
+        self.user_agent_list = [
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+            "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+        ]
+
+    def get(self, url, timeout = 3, proxy = None, num_retries = 6):
+        UA = random.choice(self.user_agent_list)
+        headers = {'User-Agent': UA, 'Referer':'http://www.quanshuwang.com/'} #不赋值防爬虫
+        if proxy == None:
+            try:
+                return requests.get(url, headers=headers, timeout=timeout)
+            except:
+                if num_retries > 0:
+                    # time.sleep(3)
+                    print('proxy == None 获取网页出错。10秒后将获取倒数第：', num_retries, '次')
+                    return self.get(url, timeout, num_retries - 1)
+                else:
+                    print("开始使用代理")
+                    time.sleep(10)
+                    IP = ''.join(str(random.choice(self.ip_list)).strip())
+                    proxy = {'http': IP}
+                    print("IP:",IP)
+                    return self.get(url, timeout, proxy)
+        else:
+            try:
+                IP = ''.join(str(random.choice(self.ip_list)).strip())
+                proxy = {'http': IP}
+                return requests.get(url, headers=headers, timeout=timeout)
+            except:
+                if num_retries > 0:
+                    time.sleep(3)
+                    IP = ''.join(str(random.choice(self.ip_list)).strip())
+                    proxy = {'http': IP}
+                    print('proxy == you 正在更换代理，10S后将重新获取倒数第', num_retries, '次')
+                    print('当前代理是：', proxy)
+                    return self.get(url, timeout, proxy, num_retries - 1)
+                else:
+                    print('代理失败，正在重试')
+                    return self.get(url, 3)
+
+request = download()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spider_2.1.py
@@ -0,0 +1,147 @@
+﻿#!/usr/bin/env Python
+# coding=utf-8
+import urllib.request as urllib
+import requests, re, operator
+from bs4 import BeautifulSoup
+# from functools import reduce #列表去重已经优化
+from enum import Enum, IntEnum, unique
+from download import request
+from connect_mysql import sql
+
+
+@unique
+class Novel_Type(IntEnum):
+    XUANHUAN =  1   #玄幻魔法
+    WUXIA    =  2   #武侠修真
+    CHUNAI   =  3   #纯爱耽美
+    DUSHI    =  4   #都市言情
+    ZHICHANG =  5   #职场校园
+    CHUANYUE =  6   #穿越重生
+    LISHI    =  7   #历史军事
+    WANGYOU  =  8   #网游动漫
+    KONGBU   =  9   #恐怖灵异
+    KEHUAN   = 10   #科幻小说
+    MEIWEN   = 11   #没问名著
+
+@unique
+class Novel_Info(IntEnum):
+    ID     = 0
+    NAME   = 1
+    TYPE   = 2
+    SERI   = 3
+    AUTHOR = 4
+    URL    = 5
+    SUM    = 6
+
+
+url = 'http://www.quanshuwang.com'
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) " \
+                   "AppleWebKit/537.36 (KHTML, like Gecko) " \
+                   "Chrome/58.0.3029.110 " \
+                   "Safari/537.36"
+}
+
+
+
+
+def getHTMLText(url, code="utf-8"): #获取网页源码 默认code是utf-8
+    try:
+        r = request.get(url, 3)
+        r.raise_for_status()
+        r.encoding = code
+        return r.text
+    except:
+        print("获取网页源代码失败")
+
+
+def getTypeUrl(url):
+    html = getHTMLText(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    a = soup.find_all('a')
+    for i in a:
+        try:
+            yield re.findall(r"http://www.quanshuwang.com/list/[0-1]?[0-9]_1.html", i.attrs['href'])[0]
+        except:
+            continue
+    raise StopIteration
+
+
+#优化list查重，避免数据过大list内存泄露
+def getEveryNovelurl(url):
+    href = ""
+    html = getHTMLText(url, code="gbk")
+    soup = BeautifulSoup(html, 'html.parser')
+    a = soup.find_all('a', target='_blank')
+    for i in a:
+        try:
+            temp = re.findall(r"http://www.quanshuwang.com/book_[0-9]*.html", i.attrs['href'])[0]
+            if operator.eq(href, temp) is False:
+                href = temp
+                yield temp
+        except:
+            continue
+    raise StopIteration
+
+
+def getChapterCenter(url):
+    html = getHTMLText(url, code="gbk")
+    r = r"style5\(\);</script>(.*)<script type=\"text/javascript\">style6\(\)"
+    return re.findall(r, html, re.S)
+
+def getNovelInformation(url):
+    print("成功进来了:", url)
+    html = getHTMLText(url, code="gbk")
+    soup = BeautifulSoup(html, 'html.parser')
+    img_url = soup.find_all('meta', property="og:image")[0].attrs['content']
+    temp_type = soup.find_all('meta', property="og:novel:category")[0].attrs['content']
+    novel_author = soup.find_all('meta', property="og:novel:author")[0].attrs['content']
+    novel_name = soup.find_all('meta', property="og:novel:book_name")[0].attrs['content']
+    novel_seri = soup.find_all('meta', property="og:novel:status")[0].attrs['content']
+    novel_update_time = soup.find_all('meta', property = "og:novel:update_time")[0].attrs['content']
+    novel_chapter_name = \
+        soup.find_all('meta', property="og:novel:latest_chapter_name")[0].attrs['content']
+    novel_chapter_url = \
+        soup.find_all('meta', property="og:novel:latest_chapter_url")[0].attrs['content']
+    novel_chapter_content = getChapterCenter(novel_chapter_url)
+    print(img_url)
+    print(temp_type)
+    print(novel_author)
+    print(novel_name)
+    print(novel_seri)
+    print(novel_update_time)
+    print(novel_chapter_name)
+    print(novel_chapter_url)
+    print(novel_chapter_content)
+
+
+if __name__ == '__main__':
+    mysql = sql
+    try:
+        for i in getTypeUrl(url): #捕获12个小说类型的url
+            print("novel_url_type: ", i)
+            for j in getEveryNovelurl(i):
+                getNovelInformation(j)
+    except:
+        print("程序异常结束")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
