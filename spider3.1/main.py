57aaeb389fa78e5b0cc1cff37becca0419d5b13d
yaoshifeng <1320701765@qq.com>
2018-04-25 14:10:42 +0800

    Add files via upload

---
+++ spider3.1/main.py
@@ -0,0 +1,36 @@
+﻿#!/usr/bin/env Python
+# coding=utf-8
+from paqu import paqu
+
+
+def main():
+    spider = paqu
+    spider.run()
+
+
+if __name__ == '__main__':
+    main()
+    print("--------------end--------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
